6fe17cd5ce5c85e019abe957df3e1ef7904bb983
George Wood <git@gwood.dev>
2025-06-11 21:14:08 +0100

    Initial Commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// Import custom CSS
+import '../scss/styles.scss';
+
+// Import all of Bootstrap’s JS
+import * as bootstrap from 'bootstrap';
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin')
+
+const glob = require('glob')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/main.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html', minify: 'auto' }),
+        new miniCssExtractPlugin(),
+        new PurgeCSSPlugin({
+            paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`, { nodir: true }),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Extracts CSS for each JS file that includes CSS
+                        loader: miniCssExtractPlugin.loader
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    autoprefixer
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                // Silence Sass deprecation warnings
+                                silenceDeprecations: [
+                                    'mixed-decls',
+                                    'color-functions',
+                                    'global-builtin',
+                                    'import'
+                                ]
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
